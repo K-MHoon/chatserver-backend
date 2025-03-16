02b1baa9788f4f59dd700be26219d761d1b49d37
K-MHOON <azmria117@gmail.com>
2025-03-16 19:14:01 +0900

    Initial commit

---
+++ src/test/java/com/kmhoon/chatserver/ChatserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kmhoon.chatserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
