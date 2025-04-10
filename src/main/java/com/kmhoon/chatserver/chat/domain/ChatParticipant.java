576494a1ee3b8c488beac421ef64ba22d5acb2cd
K-MHOON <azmria117@gmail.com>
2025-04-10 23:45:29 +0900

    feat : 채팅 참여자 엔티티 추가

---
+++ src/main/java/com/kmhoon/chatserver/chat/domain/ChatParticipant.java
@@ -0,0 +1,29 @@
+package com.kmhoon.chatserver.chat.domain;
+
+import com.kmhoon.chatserver.member.domain.BaseTimeEntity;
+import com.kmhoon.chatserver.member.domain.Member;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
+public class ChatParticipant extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chat_room_id", nullable = false)
+    private ChatRoom chatRoom;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+}
