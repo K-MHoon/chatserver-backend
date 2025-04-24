9fd6413822aa188bd41460f4fb682d1d1275b15c
K-MHOON <azmria117@gmail.com>
2025-04-24 23:27:52 +0900

    feat : 채팅 메시지 Dto 추가

---
+++ src/main/java/com/kmhoon/chatserver/chat/dto/ChatMessageDto.java
@@ -0,0 +1,16 @@
+package com.kmhoon.chatserver.chat.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ChatMessageDto {
+    private String message;
+    private String senderEmail;
+    private Long roomId;
+}
