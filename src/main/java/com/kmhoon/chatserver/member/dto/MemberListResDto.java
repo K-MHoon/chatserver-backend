4f252730e54adac9c7a065b763c61c6be32fcabd
K-MHOON <azmria117@gmail.com>
2025-04-28 23:35:18 +0900

    feat : 사용자 목록 응답 Dto 추가

---
+++ src/main/java/com/kmhoon/chatserver/member/dto/MemberListResDto.java
@@ -0,0 +1,17 @@
+package com.kmhoon.chatserver.member.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MemberListResDto {
+
+    private Long id;
+    private String name;
+    private String email;
+}
